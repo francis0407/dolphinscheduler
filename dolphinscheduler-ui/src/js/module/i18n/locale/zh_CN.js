11cb2589f42a433968a52ac96a82b2e96ff501e1
yc <bling_hong@163.com>
2021-09-23 22:50:33 +0800

    update audit log type name

--- dolphinscheduler-ui/src/js/module/i18n/locale/zh_CN.js
+++ dolphinscheduler-ui/src/js/module/i18n/locale/zh_CN.js
@@ -709,9 +709,9 @@ export default {
   'Reference from': '使用已有任务',
   'No more...': '没有更多了...',
   'Audit Log': '审计日志',
-  'Module': '模块',
-  'AllModules': '所有模块',
+  'AuditType': '审计类型',
+  'AllTypes': '所有类型',
   'AllOperations': '所有操作',
-  'User Module': '用户模块',
-  'Project Module': '项目模块'
+  'UserAudit': '用户管理审计',
+  'ProjectAudit': '项目管理审计'
 }
